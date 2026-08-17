3d21ce19b3829963835205913886c41daf2e2839
Farah-Tanveer <farahtanveer518@gmail.com>
2026-08-17 13:34:37 +0500

    Added product dbset

--- MiniStore/Models/Product.cs
+++ MiniStore/Models/Product.cs
@@ -1,11 +1,13 @@
-﻿namespace MiniStore.Models;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiniStore.Models;
 
 public class Product
 {
     public int Id { get; set; }
 
     public string Name { get; set; }
-
+    [Precision(18, 2)]
     public decimal Price { get; set; }
 
     public int Quantity { get; set; }
