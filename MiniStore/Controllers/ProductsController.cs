b3797efdad9c854a3381594204e6fcf938eda54a
Farah-Tanveer <farahtanveer518@gmail.com>
2026-08-17 16:22:25 +0500

    Added products controller

---
+++ MiniStore/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniStore.Data;
+using MiniStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiniStore.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    public ProductsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var products = await _context.Products.ToListAsync();
+        return View(products);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(product);
+    }
+}
+
